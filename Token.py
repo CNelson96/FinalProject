01453d86bf3fa7eacfc6f509f3f51fbcbd3fe252
CNelson96 <cory.nelson1199@gmail.com>
2026-05-01 12:57:17 -0500

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,3 +1,18 @@
+# I certify that the Python file I am submitting is all my own work.
+# None of it is copied from any source or any person.
+# Signed: Cory Nelson, Marcus Her, Nou Chai Thao.
+# Date: 5/01/2026
+#
+# Author: Cory Nelson, Marcus Her, Nou Chai Thao.
+# Date:  5/01/2026
+# Class: CSS335
+# Assignment: Final Project Banking
+# File Name: Token.py
+# Description: Creating the tokenizing for the banking program
+#
+
+
+
 # Token type constants — ALL_CAPS means treat as constants
 TOKEN_ACCOUNT_NUMBER = "ACCOUNT_NUMBER"  # e.g. CN123456
 TOKEN_MENU_CHOICE    = "MENU_CHOICE"     # digits 1-6
